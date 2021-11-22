249021f5462c0a0184612591bafc480150138669
defermelowie <lowiecode@gmail.com>
2021-11-22 15:20:29 +0100

    wip

--- A4/gbp_monitor.sv
+++ A4/gbp_monitor.sv
@@ -36,7 +36,7 @@ class gbp_monitor;
             probe = new({prev_A, flags});
             if (valid) begin
                 //$display("[%t | MON] Recieved: Opcode: %02x", $time, ifc.opcode);
-                $display("[%t | MON] Recieved: %s opcode: %02x", $time, probe.toString(), opcode.opcode);
+                $display("[%t | MON] Recieved: %s opcode: %02x", $time, probe.toString(), ifc.opcode);
                 mon2che.put(probe);
             end
         end
