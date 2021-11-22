5920425bafa57e051211da89b5e360103935d5de
defermelowie <lowiecode@gmail.com>
2021-11-22 14:27:57 +0100

    wip

--- A4/gbp_monitor.sv
+++ A4/gbp_monitor.sv
@@ -16,11 +16,18 @@ class gbp_monitor;
     task run();
         probe probe;
 
+        byte A;
+        byte prev_A;
+        byte flags;
+
         $display($sformatf("[%t | MON] Started monitoring", $time));
 
         forever begin
             @(negedge this.ifc.clock);
-            probe = new(ifc.probe);
+            prev_A = ifc.probe[15:8];
+            flags = ifc.probe[7:0];
+            probe = new({A, flags});
+            $$display("[%t | MON] Recieved: %s", $time, probe.toString());
             mon2che.put(probe);
         end
     endtask : run
