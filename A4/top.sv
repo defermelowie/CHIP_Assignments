d6459e94db6ef0692f2b490dd5aa94c8e71767a6
lowie <defermelowie@gmail.com>
2021-11-23 10:43:41 +0100

    Commented out unnecessary bins

--- A4/top.sv
+++ A4/top.sv
@@ -32,50 +32,50 @@ module top;
 
         operation: coverpoint theInterface.opcode[7:3]
             iff(theInterface.valid) {
-                bins add = {'h10};
+                //bins add = {'h10};
                 bins adc = {'h11};
-                bins sub = {'h12};
+                //bins sub = {'h12};
                 bins sbc = {'h13};
-                bins andi = {'h14};
-                bins xori = {'h15};
-                bins ori = {'h16};
+                //bins andi = {'h14};
+                //bins xori = {'h15};
+                //bins ori = {'h16};
                 bins cpi = {'h17};
             }
 
-        register: coverpoint theInterface.opcode[2:0] 
-            iff(theInterface.valid){
-                bins b = {'h0};
-                bins c = {'h1};
-                bins d = {'h2};
-                bins e = {'h3};
-                bins h = {'h4};
-                bins l = {'h5};
-                bins hl = {'h6};
-                bins a = {'h7};
-            }
+        //register: coverpoint theInterface.opcode[2:0] 
+        //    iff(theInterface.valid){
+        //        bins b = {'h0};
+        //        bins c = {'h1};
+        //        bins d = {'h2};
+        //        bins e = {'h3};
+        //        bins h = {'h4};
+        //        bins l = {'h5};
+        //        bins hl = {'h6};
+        //        bins a = {'h7};
+        //    }
 
         Z_flag: coverpoint theInterface.probe[7]
             iff(theInterface.valid) {
                 bins high = {'b1};
-                bins low = {'b0};
+                //bins low = {'b0};
             }
 
         N_flag: coverpoint theInterface.probe[6]
             iff(theInterface.valid) {
                 bins high = {'b1};
-                bins low = {'b0};
+                //bins low = {'b0};
             }
 
         H_flag: coverpoint theInterface.probe[5]
             iff(theInterface.valid) {
                 bins high = {'b1};
-                bins low = {'b0};
+                //bins low = {'b0};
             }
 
         C_flag: coverpoint theInterface.probe[4]
             iff(theInterface.valid) {
                 bins high = {'b1};
-                bins low = {'b0};
+                //bins low = {'b0};
             }
             
         cx: cross C_flag, operation{
